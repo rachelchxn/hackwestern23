9473591f87a5c5a2b80887dedbccd53613640dd5
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-11-25 15:01:04 -0500

    james got it working

--- functions/firebase.py
+++ functions/firebase.py
@@ -1,4 +1,8 @@
 import firebase_admin
+import cv2
+from PIL import Image
+import io
+import numpy as np
 
 cred = firebase_admin.credentials.Certificate('hw23-e0512-firebase-adminsdk-3ax9k-293086f6f4.json')
 firebase_admin.initialize_app(cred)
@@ -37,7 +41,18 @@ def uploadImageFromBlob(blob, image_name):
     
     bucket = storage.bucket("hw23-e0512.appspot.com")
     blob = bucket.blob(image_name)
-    blob.upload_from_string(blob)
+    image = blob[0].transpose(1, 2, 0)  # Convert from (channels, height, width) to (height, width, channels)
+    image = np.uint8(image)  # Convert to unsigned byte format
+
+    # Convert to PIL Image
+    pil_image = Image.fromarray(image)
+
+    # Convert to Byte Stream for uploading
+    byte_stream = io.BytesIO()
+    pil_image.save(byte_stream, format='JPEG')
+    byte_stream.seek(0)
+
+    blob.upload_from_file(byte_stream, content_type='image/jpeg')
 
     return blob.public_url
 
