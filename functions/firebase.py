d35dc636ed323c7d4a97805dd84554a335c71763
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-11-25 18:27:56 -0500

    transcript.py working

---
+++ functions/firebase.py
@@ -0,0 +1,61 @@
+from firebase_admin import firestore, storage
+import firebase_admin
+import cv2
+from PIL import Image
+import io
+import numpy as np
+
+cred = firebase_admin.credentials.Certificate('hw23-e0512-firebase-adminsdk-3ax9k-293086f6f4.json')
+firebase_admin.initialize_app(cred)
+
+
+# Get a reference to the Firestore service
+db = firestore.client()
+
+# write into the people collection
+def create_document(data, document_name = None, collection_name = 'people'):
+    if document_name is None:
+        doc_ref = db.collection(collection_name).document()
+    else:
+        doc_ref = db.collection(collection_name).document(document_name)
+    doc_ref.set(data)
+    return doc_ref.id
+
+def read_document(document_id, collection_name = 'people'):
+    doc_ref = db.collection(collection_name).document(document_id)
+    doc = doc_ref.get()
+    return doc.to_dict()
+
+# upload image to storage
+def uploadImageFromPath(path, image_name):
+    print("uploading image...")
+    
+    bucket = storage.bucket("hw23-e0512.appspot.com")
+    blob = bucket.blob(image_name)
+    blob.upload_from_filename(path)
+
+    return blob.public_url
+
+def uploadImageFromBlob(blob, image_name):
+    print("uploading image...")
+    
+    bucket = storage.bucket("hw23-e0512.appspot.com")
+    blob = bucket.blob(image_name)
+    image = blob[0].transpose(1, 2, 0)  # Convert from (channels, height, width) to (height, width, channels)
+    image = np.uint8(image)  # Convert to unsigned byte format
+
+    # Convert to PIL Image
+    pil_image = Image.fromarray(image)
+
+    # Convert to Byte Stream for uploading
+    byte_stream = io.BytesIO()
+    pil_image.save(byte_stream, format='JPEG')
+    byte_stream.seek(0)
+
+    blob.upload_from_file(byte_stream, content_type='image/jpeg')
+
+    return blob.public_url
+
+# upload image to storage
+print(uploadImageFromPath('images/james.jpg', "test"))
+
---
+++ transcribe.py
@@ -0,0 +1,74 @@
+import requests
+import base64
+import json
+
+import pyaudio
+import wave
+import io
+
+def record_audio(duration=5, sample_rate=48000, channels=1):
+    audio_format = pyaudio.paInt16
+    chunk = 1024
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=audio_format, channels=channels,
+                    rate=sample_rate, input=True,
+                    frames_per_buffer=chunk)
+
+    print("Recording...")
+
+    frames = []
+
+    for _ in range(0, int(sample_rate / chunk * duration)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("Finished recording.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    buffer = io.BytesIO()
+    wf = wave.open(buffer, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(audio_format))
+    wf.setframerate(sample_rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    buffer.seek(0)
+    return buffer.read()
+
+
+def transcribe(api_key, language_code="en-US"):
+    url = f"https://speech.googleapis.com/v1/speech:recognize?key={api_key}"
+
+    audio_content = base64.b64encode(record_audio()).decode('UTF-8')
+
+    # Prepare request payload
+    data = {
+        "config": {
+            "encoding": "LINEAR16",
+            "sampleRateHertz": 48000,  # Match this with your audio file's sample rate
+            "languageCode": language_code
+        },
+        "audio": {
+            "content": audio_content
+        }
+    }
+
+    # Send the POST request
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Error code {response.status_code}: {response.text}")
+
+# Usage example
+api_key = "AIzaSyBN197RE1Jgm3F_oQ8OkRIXg9RB6xPXL3g"
+audio_file_path = "test.wav"
+response = transcribe(api_key)
+print(json.dumps(response, indent=2))
