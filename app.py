9789fed85403cc6fc00f969f8e78eead7bd43bf5
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-11-25 23:16:03 -0500

    duplicates fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import speech_recognition as sr
 import threading
 import pyaudio
 import uuid
-import io
+import os
 from PIL import Image
 
 from firebase_admin import credentials, firestore, storage
@@ -35,31 +35,9 @@ def find_similar_face_key(face_encoding, faces_dict, tolerance=0.6):
             return face_key
     return None
 
-# def uploadImageFromBlob(imageblob, image_name):
-#     print("uploading image...")
-    
-#     # bucket = storage.bucket("hw23-e0512.appspot.com")
-#     bucket = storage.bucket()
-#     blob = bucket.blob(image_name)
-#     image = imageblob[0].transpose(1, 2, 0)  # Convert from (channels, height, width) to (height, width, channels)
-#     image = np.uint8(image)  # Convert to unsigned byte format
-
-#     # Convert to PIL Image
-#     pil_image = Image.fromarray(image)
-
-#     # Convert to Byte Stream for uploading
-#     byte_stream = io.BytesIO()
-#     pil_image.save(byte_stream, format='JPEG')
-#     byte_stream.seek(0)
-
-#     blob.upload_from_file(byte_stream, content_type='image/jpeg')
-
-#     return blob.public_url
-
-
 # Dictionary to track unrecognized faces
 unrecognized_faces = {}
-unrecognized_threshold = 3  # Number of frames to confirm an unrecognized face
+unrecognized_threshold = 4  # Number of frames to confirm an unrecognized face
 
 video_capture = cv2.VideoCapture(1)
 
@@ -70,7 +48,6 @@ def camera_operations(video_capture):
 
     while True:
         ret, frame = video_capture.read()
-        # print(frame)
         if not ret:
             break
 
@@ -81,6 +58,7 @@ def camera_operations(video_capture):
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
+
         for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
             top *= 4
             right *= 4
@@ -93,50 +71,72 @@ def camera_operations(video_capture):
             left = max(left, 0)
 
             face_image = frame[top:bottom, left:right]
-            
-            if face_image.size > 0:
+
+            if face_image.size > 0:       
                 matches = face_recognition.compare_faces(face_encodings_from_db, face_encoding, tolerance=0.5)
+
                 if True in matches:
                     first_match_index = matches.index(True)
                     name = face_names_from_db[first_match_index]
+                    similar_key = find_similar_face_key(face_encoding, unrecognized_faces)
+                    if similar_key is not None:
+                        del unrecognized_faces[similar_key]  # Remove from unrecognized as it's now recognized
                 else:
-                    # Generate unique ID for the image and upload it
-                    unique_id = str(uuid.uuid4())
-                    image_path = f"faces/{unique_id}.jpg"
-                    # save the frame into the images folder
-                    cv2.imwrite(image_path, face_image)
-                    image_url = uploadImageFromPath(image_path, image_path)
-                    print(image_url)
+                    similar_key = find_similar_face_key(face_encoding, unrecognized_faces)
 
-                    # Save the encoding and image URL in Firestore
-                    doc_ref = db.collection('people').document(unique_id)
-                    doc_ref.set({
-                        'image_enc': face_encoding.tolist(),
-                        'name': "Unnamed Person",
-                        'image_url': image_url,
-                        'timestamp': SERVER_TIMESTAMP 
-                    })
+                    if similar_key is not None:
+                        unrecognized_faces[similar_key]['counter'] += 1
+                        print(f"HERE:{unrecognized_faces[similar_key]['counter']}")
+                    else:
+                        print("New face detected, starting counter")
+                        unrecognized_faces[tuple(face_encoding)] = {'counter': 1, 'encoding': face_encoding}
 
-                    # Update local encodings for real-time comparison
-                    face_encodings_from_db.append(face_encoding)
-                    face_names_from_db.append("Unnamed Person")
+                    if similar_key is not None and unrecognized_faces[similar_key]['counter'] > unrecognized_threshold:
+                        # Process new unrecognized face
+                        name = "Unnamed Person"
+                        face_encoding_list = unrecognized_faces[similar_key]['encoding'].tolist()
+                        doc_ref = db.collection('people').document()
 
-                    name = "Unnamed Person"
+                        unique_id = str(uuid.uuid4())
+                        image_path = f"faces/{unique_id}.jpg"
+                        cv2.imwrite(image_path, face_image)
+                        image_url = uploadImageFromPath(image_path, image_path)
 
-                face_names.append(name)
+                        doc_ref = db.collection('people').document(unique_id)
+                        doc_ref.set({
+                            'image_enc': face_encoding_list,
+                            'name': "Unnamed Person",
+                            'image_url': image_url,
+                            'timestamp': SERVER_TIMESTAMP 
+                        })
+                        os.remove(image_path)
 
-            # Drawing the results on the frame
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                        # Update local encodings for real-time comparison
+                        face_encodings_from_db.append(face_encoding)
+                        face_names_from_db.append("Unnamed Person")
 
-            cv2.imshow('Video', frame)
+                        name = "Unnamed Person"   
+                        del unrecognized_faces[similar_key]
+                        
+                    else:
+                        name = "[Checking...]"
+
+                if name:
+                    face_names.append(name)
+
+
+                # Drawing the results on the frame
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
             
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
