6dd3b4192278de7b913e1f86c705c9335816e7fd
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-11-26 03:39:46 -0500

    open ai in transcript

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import threading
 import cv2
-from transcribe import transcribe, stop_recording
+from transcribe import summarize, transcribe, stop_recording
 from app import camera_operations
 import time
 
@@ -10,28 +10,30 @@ def main():
     def handle_transcription():
         transcript_parts = transcribe(api_key)
         full_transcript = "".join(transcript_parts)
+        summary = summarize(full_transcript)
         print(full_transcript)
+        print(summary.choices[0].message.content)
 
     # Transcription thread
     transcription_thread = threading.Thread(target=handle_transcription)
     transcription_thread.start()
 
     # Camera operations thread
-    video_capture = cv2.VideoCapture(1) 
-    camera_thread = threading.Thread(target=lambda: camera_operations(video_capture))
-    camera_thread.start()
+    # video_capture = cv2.VideoCapture(1) 
+    # camera_thread = threading.Thread(target=lambda: camera_operations(video_capture))
+    # camera_thread.start()
 
     try:
         # For demonstration, let's wait for 10 seconds and then stop recording
-        time.sleep(10)
+        time.sleep(15)
         stop_recording()
     except Exception as e:
         print(f"An error occurred: {e}")
 
     # Wait for both threads to finish
-    transcription_thread.join()
-    camera_thread.join()
-    video_capture.release()
+    # transcription_thread.join()
+    # camera_thread.join()
+    # video_capture.release()
 
 if __name__ == "__main__":
     main()
